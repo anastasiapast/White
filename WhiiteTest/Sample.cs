a6cebdc37658f45c0e7878b43b482afafd07d82b
Anastasia Pastukhova <apastukhova@rocketsoftware.com>
2018-11-12 16:47:25 +0500

    Sample added

---
+++ WhiiteTest/Sample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.IO;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WindowStripControls;
+using Assert = NUnit.Framework.Assert;
+using System.Collections;
+
+namespace WhiiteTest
+{
+    [TestFixture]
+    public class Sample
+    {
+        [TestCaseSource("SumTest")]
+        public void summ(int a, int b, int result)
+        {
+            Assert.AreEqual(a + b, result);
+        }
+
+
+        static object[] SumTest =
+        {
+            new object[] { 12, 3, 11 },
+            new object[] { 12, 2, 14 },
+            new object[] { 12, 4, 16 }
+        };
+    }
+}
